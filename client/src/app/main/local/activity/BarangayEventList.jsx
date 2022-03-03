6019983db51fa9e70f5097a6050a3a3e6a5dc614
Kenneth <kennfamarin@gmail.com>
2022-03-03 13:26:31 +0800

    Added Attend and Leave(modal) button

--- client/src/app/main/local/activity/BarangayEventList.jsx
+++ client/src/app/main/local/activity/BarangayEventList.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import './BarangayEventList.css';
-import { Collapse } from 'antd';
-import { Card } from 'antd';
-import { Button } from 'antd';
 import logo from './barangay-lumil.png';
+import { useState } from 'react';
+import { Modal, Button, Collapse, Card, Form, Input, Select } from 'antd';
 
+const { Option } = Select;
+const formItemLayout = {};
 
 const BarangayEventList = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
     const { Panel } = Collapse;
 
     function callback(key) {
         console.log(key);
         }
+
+        const [form] = Form.useForm();
+        const prefixSelector = (
+          <Form.Item name="prefix" noStyle>
+            <Select
+              style={{
+                width: 70,
+              }}
+            >
+              <Option value="86">+63</Option>
+              <Option value="87">+87</Option>
+            </Select>
+          </Form.Item>
+        );
+    
+        
 
             return(
 
@@ -25,6 +56,47 @@ const BarangayEventList = () => {
                             </div>
                         </div>
 
+                        <Modal title="Leave Event" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                            <p>The following are needed to be fill'ed to leave event</p>
+                            <Form>
+                                <Form.Item
+                                    name="email"
+                                    label="E-mail"
+                                    rules={[
+                                    {
+                                        type: 'email',
+                                        message: 'The input is not valid E-mail!',
+                                    },
+                                    {
+                                        required: true,
+                                        message: 'Please input your E-mail!',
+                                    },
+                                    ]}
+                                >
+                                    <Input />
+                                </Form.Item>
+
+                                <Form.Item
+                                    name="phone"
+                                    label="Phone Number"
+                                    rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your phone number!',
+                                    },
+                                    ]}
+                                >
+                                    <Input
+                                    addonBefore={prefixSelector}
+                                    style={{
+                                        width: '100%',
+                                    }}
+                                    />
+                                </Form.Item>
+
+                            </Form>
+                        </Modal>
+
                         <div className="card-wrapper1">
                             <Card title="Event" bordered={false} style={{width:  "100%" }}>
                             
@@ -32,7 +104,21 @@ const BarangayEventList = () => {
                                 <Panel  showArrow={false} header="Libreng Tuli sa Puting Kahoy" key="1">
                                     <p> Lorem ipsum dolor sit amet. Id earum quia quo perspiciatis commodi sed cupiditate libero. Aut aperiam sunt sed quia consequatur qui numquam itaque vel quibusdam temporibus. In possimus nisi ut voluptatem accusantium id omnis consequatur qui fugiat aspernatur 33 totam galisum!</p>
                                     <Button type="link">See More...</Button>
+
+                                    <div className="event-btn">
+                                        <div className="attend-btn">
+                                            <Button type="primary">
+                                                Attend
+                                            </Button>
+                                        </div>
+                                        <div className="leave-btn">
+                                            <Button type="primary" onClick={showModal}>
+                                                Leave
+                                            </Button>
+                                        </div>
+                                    </div>
                                     
+                                   
                                 </Panel>
                             </Collapse>
 
@@ -40,6 +126,20 @@ const BarangayEventList = () => {
                                 <Panel  showArrow={false} header="Libreng Bakuna sa Puting Kahoy" key="2">
                                     <p> Lorem ipsum dolor sit amet. Id earum quia quo perspiciatis commodi sed cupiditate libero. Aut aperiam sunt sed quia consequatur qui numquam itaque vel quibusdam temporibus. In possimus nisi ut voluptatem accusantium id omnis consequatur qui fugiat aspernatur 33 totam galisum!</p>
                                     <Button type="link">See More...</Button>
+
+                                    <div className="event-btn">
+                                        <div className="attend-btn">
+                                            <Button type="primary">
+                                                Attend
+                                            </Button>
+                                        </div>
+                                        <div className="leave-btn">
+                                            <Button type="primary" onClick={showModal}>
+                                                Leave
+                                            </Button>
+                                        </div>
+                                    </div>
+
                                 </Panel>
                             </Collapse>
 
@@ -55,6 +155,20 @@ const BarangayEventList = () => {
                                 <Panel  showArrow={false} header="Libreng Tuli sa Puting Kahoy" key="1">
                                     <p> Lorem ipsum dolor sit amet. Id earum quia quo perspiciatis commodi sed cupiditate libero. Aut aperiam sunt sed quia consequatur qui numquam itaque vel quibusdam temporibus. In possimus nisi ut voluptatem accusantium id omnis consequatur qui fugiat aspernatur 33 totam galisum!</p>
                                     <Button type="link">See More...</Button>
+
+                                    <div className="event-btn">
+                                        <div className="attend-btn">
+                                            <Button type="primary">
+                                                Attend
+                                            </Button>
+                                        </div>
+                                        <div className="leave-btn">
+                                            <Button type="primary" onClick={showModal}>
+                                                Leave
+                                            </Button>
+                                        </div>
+                                    </div>
+
                                 </Panel>
                             </Collapse>
 
@@ -62,6 +176,20 @@ const BarangayEventList = () => {
                                 <Panel  showArrow={false} header="Libreng Bakuna sa Puting Kahoy" key="2">
                                     <p> Lorem ipsum dolor sit amet. Id earum quia quo perspiciatis commodi sed cupiditate libero. Aut aperiam sunt sed quia consequatur qui numquam itaque vel quibusdam temporibus. In possimus nisi ut voluptatem accusantium id omnis consequatur qui fugiat aspernatur 33 totam galisum!</p>
                                     <Button type="link">See More...</Button>
+
+                                    <div className="event-btn">
+                                        <div className="attend-btn">
+                                            <Button type="primary">
+                                                Attend
+                                            </Button>
+                                        </div>
+                                        <div className="leave-btn">
+                                            <Button type="primary" onClick={showModal}>
+                                                Leave
+                                            </Button>
+                                        </div>
+                                    </div>
+
                                 </Panel>
                             </Collapse>
 
